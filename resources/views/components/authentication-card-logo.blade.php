8db283020fba24ff770019336a2ddd5c99e863e7
Raúl López <hi@raullg.com>
2023-11-24 15:21:15 +0100

    improve auth pages

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,3 +1,3 @@
 <a href="/">
-    <x-application-logo class="w-20 h-20" />
+    <x-application-logo class="w-20 h-20" :name="true" nameClass="text-2xl text-white font-bold pl-4" />
 </a>
--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,11 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
-    <div>
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-body">
+    <img class="absolute bottom-0 left-0 w-full pointer-events-none -z-1"
+        src="{{ asset('casper-assets/headers/bg-bottom-lines.png') }}" alt="">
+    <div class="relative">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-body overflow-hidden sm:rounded-lg relative">
         {{ $slot }}
     </div>
 </div>
