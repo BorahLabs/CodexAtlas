ce845d0ddb5c703dc9fa71a5cd7c6a1c06fad737
Raúl López <hi@raullg.com>
2023-12-16 23:00:22 +0000

    Update 2023_11_19_141646_create_system_components_table.php

--- database/migrations/2023_11_19_141646_create_system_components_table.php
+++ database/migrations/2023_11_19_141646_create_system_components_table.php
@@ -24,7 +24,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->unique(['branch_id', 'path']);
-            $table->unique(['branch_id', 'sha']);
+            $table->index(['branch_id', 'sha']);
         });
     }
 
