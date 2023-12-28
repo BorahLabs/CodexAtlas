673883b92641163037fff543fd584ea6424d0090
Raúl López <hi@raullg.com>
2023-12-28 00:32:18 +0000

    feat: add pricing to homepage

---
+++ resources/views/components/homepage/pricing-not-included.blade.php
@@ -0,0 +1,9 @@
+<li class="flex items-center">
+    <div class="flex mr-2 items-center justify-center w-5 h-5 rounded-full bg-red-600 flex-shrink-0">
+        <svg width="12" height="12" viewbox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M9 3L3 9" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+            <path d="M3 3L9 9" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+        </svg>
+    </div>
+    <span class="text-base text-white tracking-tight">{{ $text }}</span>
+</li>
