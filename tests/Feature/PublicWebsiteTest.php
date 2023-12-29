e67953e12d80be5d2948bde3cb973641b5e843b2
Raúl López <hi@raullg.com>
2023-12-29 16:50:35 +0000

    Update PublicWebsiteTest.php

--- tests/Feature/PublicWebsiteTest.php
+++ tests/Feature/PublicWebsiteTest.php
@@ -11,3 +11,18 @@ it('has a terms and conditions page', function () {
 it('has a privacy policy page', function () {
     $this->get(route('policy.show'))->assertStatus(200);
 });
+
+it('can be indexed', function () {
+    config(['app.indexable' => true]);
+    $this->get('/')->assertHeaderMissing('X-Robots-Tag');
+});
+
+it('can\'t be indexed if env variable', function () {
+    config(['app.indexable' => false]);
+    $this->get('/')->assertHeader('X-Robots-Tag', 'noindex, nofollow, noarchive, nosnippet, noodp, notranslate, noimageindex');
+});
+
+it('redirects to secret page if password protected', function () {
+    config(['app.password_protected.enabled' => true]);
+    $this->get('/')->assertRedirect(route('password-protected'));
+});
