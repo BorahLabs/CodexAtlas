ee80a5dd91c632a124468d95407b3e79f9365b83
ismaelilloDev <127984176+ismaelilloDev@users.noreply.github.com>
2024-03-16 12:13:30 +0100

    test goes to the correct flow

--- config/services.php
+++ config/services.php
@@ -62,9 +62,9 @@ return [
             'secret_key' => env('TURNSTILE_SECRET_KEY'),
         ],
     'ngrok' => [
-        'active_helper' => false, // put it to true if you are using ngrok
+        'active_helper' => false, // put it to true if you are using ngrok and  ALWAYS SHOULD BE FALSE
         'user_id' => '9b22f325-516a-48b2-8ffa-9d724a08267c', //userId to automatically login
-        'ngrok_domain' => 'https://ca49-88-25-31-8.ngrok-free.app' //ngrok url
+        'ngrok_domain' => 'https://cff9-88-25-31-8.ngrok-free.app' //ngrok url
     ],
 
     'gh' => [
