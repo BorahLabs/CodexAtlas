ec9ff0700730d86c8978f391df128b3ba6c4adf2
Raúl López <hi@raullg.com>
2024-05-24 19:26:06 +0100

    set kw

--- resources/views/components/homepage/footer.blade.php
+++ resources/views/components/homepage/footer.blade.php
@@ -45,7 +45,7 @@
             <div class="mb-10">
                 <a href="{{ route('tools.code-fixer') }}"
                     class="font-bold text-violet-50 mb-4 uppercase flex items-center justify-start">
-                    <span>Free AI Code Fixer</span>
+                    <span>Fix my code with AI</span>
                 </a>
             </div>
             <div class="pt-10 border-t border-newGray-800">
