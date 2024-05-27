2402b5c550b15c4c1f927e00554208b4bdc775b6
Raúl López <hi@raullg.com>
2024-05-27 12:13:18 +0100

    feat: remove crawler from sitemap

--- app/Http/Controllers/Website/SitemapController.php
+++ app/Http/Controllers/Website/SitemapController.php
@@ -7,15 +7,16 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
-use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Sitemap;
 use Spatie\Sitemap\Tags\Url;
 
 class SitemapController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $sitemap = SitemapGenerator::create(config('app.url'))->getSitemap();
-        $cacheKey = 'codex-sitemap-v2';
+        $sitemap = Sitemap::create();
+        $sitemap->add(Url::create(route('homepage'))->setPriority(1)->setChangeFrequency('daily'));
+        $cacheKey = 'codex-sitemap-v3';
         if (Cache::has($cacheKey)) {
             $sitemapContents = Cache::get($cacheKey);
 
@@ -23,11 +24,11 @@ class SitemapController extends Controller
         }
 
         foreach (Guesser::supportedLanguages() as $language) {
-            $sitemap->add(Url::create(route('tools.code-documentation', ['language' => Str::slug($language->name())], absolute: false))->setPriority(0.9)->setChangeFrequency('weekly'));
+            $sitemap->add(Url::create(route('tools.code-documentation', ['language' => Str::slug($language->name())]))->setPriority(0.9)->setChangeFrequency('weekly'));
         }
 
         foreach (\App\CodeConverter\Tools\CodeConverterTool::all() as $tool) {
-            $sitemap->add(Url::create($tool->url(absolute: false))->setPriority(0.9)->setChangeFrequency('weekly'));
+            $sitemap->add(Url::create($tool->url())->setPriority(0.9)->setChangeFrequency('weekly'));
         }
 
         $sitemap->add(Url::create(route('tools.code-fixer', absolute: false))->setPriority(0.9)->setChangeFrequency('weekly'));
@@ -38,7 +39,7 @@ class SitemapController extends Controller
                     continue;
                 }
 
-                $sitemap->add(Url::create($file->url(absolute: false))->setPriority(0.8)->setChangeFrequency('weekly'));
+                $sitemap->add(Url::create($file->url())->setPriority(0.8)->setChangeFrequency('weekly'));
             }
         }
 
