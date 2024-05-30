9855fcd9227421d02f6200fe31d94a980c58ff52
Raúl López <hi@raullg.com>
2024-05-30 15:20:21 +0100

    improve prompts

--- app/LLM/PromptRequests/OpenAI/DocumentFilePromptRequest.php
+++ app/LLM/PromptRequests/OpenAI/DocumentFilePromptRequest.php
@@ -12,27 +12,27 @@ class DocumentFilePromptRequest implements PromptRequest
     {
         return 'You are an expert in writing software documentation. Write a short description of the provided in JSON format with the following structure:
 
-        [KEY] tldr
-        [VALUE] General overview of what the file does
-
-        [KEY] classes
-        [VALUE] A JSON with this structure:
-
-            "name": "Name of the class",
-            "description": "Small Description of the class",
-            "methods": [
-                {
-                    "name": "Name of the method",
-                    "description": "What the method does"
-                }
-            ]
+            {
+                "tldr": "General overview of what the file does",
+                "classes": [
+                    {
+                        "name": "Name of the class",
+                        "description": "Small Description of the class",
+                        "methods": [
+                            {
+                                "name": "Name of the method",
+                                "description": "What the method does"
+                            }
+                        ]
+                    }
+                ]
+            }
 
         Some rules:
-        - Response JSON Always should have key tldr.
-        - Response JSON Always should have key classes,
+        - Response should always include the key tldr.
+        - Response should always include the key classes,
         - Classes value should be an array where each item have the keys name, description and methods.
         - methods value should be an array where each item should have key name and description. If you dont find any method you can return an empty array.
-        - [KEY] Means the key of the json and the [VALUE] means the value that should be in this key.
         - Do not output the original file
         - If there are no methods or no classes, please do not include the section in the output
         - Dont add \n, Only the json is required';
