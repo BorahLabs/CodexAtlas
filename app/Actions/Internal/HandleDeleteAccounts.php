e4d92348799cf210ab0f673605786a2380192973
raulsf-dev <raulsf.dev@gmail.com>
2024-07-02 12:57:48 +0100

    implemented deleted user account and related data

---
+++ app/Actions/Internal/HandleDeleteAccounts.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Actions\Internal;
+
+use App\Actions\Codex\Architecture\SystemComponents;
+use App\Jobs\DeleteAccount;
+use App\Models\Branch;
+use App\Models\Project;
+use App\Models\ProjectConcept;
+use App\Models\Repository;
+use App\Models\SourceCodeAccount;
+use App\Models\SystemComponent;
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class HandleDeleteAccounts
+{
+    use AsAction;
+
+    public string $commandSignature = 'codex:delete-soft-users';
+
+    public function handle()
+    {
+        $users = User::query()->withTrashed()->whereNotNull('deleted_at')->where('deleted_at', '<=', now()->subDays(30))->get();
+
+        $users->each(function (User $user) {
+            DeleteAccount::dispatch($user);
+        });
+    }
+}
