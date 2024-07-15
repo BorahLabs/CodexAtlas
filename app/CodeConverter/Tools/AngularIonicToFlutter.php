8e88123b45e0d5fbdc009901b15ba554de17d6d8
Raúl López <hi@raullg.com>
2024-07-15 13:07:55 +0100

    reenable

---
+++ app/CodeConverter/Tools/AngularIonicToFlutter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\CodeConverter\Tools;
+
+use App\Atlas\Frameworks\Flutter;
+use App\Atlas\Frameworks\IonicAngular;
+
+class AngularIonicToFlutter extends CodeConverterTool
+{
+    public function __construct()
+    {
+        parent::__construct(new IonicAngular(), new Flutter());
+    }
+}
---
+++ app/CodeConverter/Tools/ReactToFlutter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\CodeConverter\Tools;
+
+use App\Atlas\Frameworks\Flutter;
+use App\Atlas\Frameworks\React;
+
+class ReactToFlutter extends CodeConverterTool
+{
+    public function __construct()
+    {
+        parent::__construct(new React(), new Flutter());
+    }
+}
